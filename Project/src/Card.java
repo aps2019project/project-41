2d13e044aeeb865241e0f101337ef7a2bddcdc49
mohammad matin fotouhi <matin.fotouhi@yahoo.com>
2019-04-30 15:24:53 +0430

    nothing!

--- Project/src/Card.java
+++ Project/src/Card.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 public class Card {
+<<<<<<< HEAD
     ArrayList<Card> cards = new ArrayList<>();
     private final int id;
     private final String name;
@@ -10,6 +11,14 @@ public class Card {
         this.name = name;
         this.price = price;
     }
+=======
+    private static ArrayList<Card> cards = new ArrayList<>();
+    private int id;
+    private String name;
+    private int price;
+
+
+>>>>>>> 71be82717f4be680b0e1fdf330cf3dbfefc4e415
     public ArrayList<Card> getCards() {
         return cards;
     }
--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner ;
 public class Main {
     public static void main(String[] args) {
+<<<<<<< HEAD
         if(Menus.loginMenu() != -1)
         {
             while(Menus.mainMenu() != -1)
@@ -9,4 +10,8 @@ public class Main {
             }
         }
     }
+=======
+        System.out.println("hello");
+   }
+>>>>>>> 71be82717f4be680b0e1fdf330cf3dbfefc4e415
 }
