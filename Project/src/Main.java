e7fc374ddaa6c5a96137e1535396bb16971d43d0
mohammad matin fotouhi <matin.fotouhi@yahoo.com>
2019-04-30 15:28:22 +0430

    solved some conflicts

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,7 +1,6 @@
 import java.util.Scanner ;
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
         if(Menus.loginMenu() != -1)
         {
             while(Menus.mainMenu() != -1)
@@ -10,8 +9,4 @@ public class Main {
             }
         }
     }
-=======
-        System.out.println("hello");
-   }
->>>>>>> 71be82717f4be680b0e1fdf330cf3dbfefc4e415
 }
