2a0d08c9408f1f6ef87bef7b01f81dc5dc0ac073
hgh <mr.crow.hgh@gmail.com>
2019-05-05 13:50:47 +0430

    item methodes name

--- Project/src/Item.java
+++ Project/src/Item.java
@@ -2,4 +2,115 @@ public class Item {
     private int id;
     private String name;
 
+
+
+
+    public void tajDanaE() {
+
+
+    }
+
+    public void namuseSepar(){
+
+    }
+
+    public void kamaneDamol(){
+
+    }
+
+
+    public void nushDaroo(){
+
+    }
+
+
+    public void tireDoShakh(){
+
+    }
+
+
+
+    public void pareSimorgh(){
+
+    }
+
+
+    public void Exir(){
+
+    }
+
+
+
+    public void majuneMana(){
+
+    }
+
+
+
+    public void majuneRuEnTani(){
+
+    }
+
+
+
+    public void nefrineMarg(){
+
+    }
+
+
+
+    public void randomDamage(){
+
+    }
+
+
+
+    public void terrorHood(){
+
+    }
+
+
+    public void bladesOFAgility(){
+
+    }
+
+    public void kingWisdom(){
+
+    }
+
+
+
+    public void assassiationDagger(){
+
+    }
+
+
+    public void poisonousDagger(){
+
+    }
+
+
+
+    public void shockHammer(){
+
+    }
+
+
+
+    public void soulEater(){
+
+    }
+
+
+    public void qosleTamid(){
+
+    }
+
+
+
+    public void shamshireChini(){
+
+    }
+
+
 }
